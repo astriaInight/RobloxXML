c8e880c96cf47f1de1c5495f274f9e505c9cd0c8
astrialnight <77846505+astriaInight@users.noreply.github.com>
2022-02-01 16:08:11 -0500

    Create XML.lua

---
+++ XML.lua
@@ -0,0 +1,33 @@
+local function parseXmlTags(input)
+	local tags = {}
+
+	for tag, content in string.gmatch(input.content, "<([%w%d]-)>(.+)</%1>") do
+		local moreContent = parseXmlTags({ tag = tag, content = content })
+		if moreContent then
+			content = moreContent
+		end
+
+		table.insert(tags, {
+			tag = tag,
+			content = content
+		})
+		print(tags)
+	end
+
+	if #tags > 0 then
+		return tags
+	else
+		return nil
+	end
+end
+
+local function parseXml(input : string)
+	local xml = {}
+
+	local formatted = { tag = "content", content = input }
+	local firstRun = parseXmlTags(formatted)
+
+	return firstRun
+end
+
+return parseXml
